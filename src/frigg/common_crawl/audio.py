8caff152ed0af3021543a2af544e7bc7e8140e05
Enrico Shippole <enricoship@gmail.com>
2025-10-02 13:10:27 -0400

    Refactor to have a base class for all

--- src/frigg/common_crawl/audio.py
+++ src/frigg/common_crawl/audio.py
@@ -1,53 +1,7 @@
-from urllib.parse import urljoin
-
-import daft
-import simdjson
-import xxhash
-from daft import col
+from frigg.common_crawl.base import Extractor
 
 
-class Audios:
-    def __init__(self):
-        pass
-
-    def __call__(self, df):
-        df = df.where(col("WARC-Type") == "metadata")
-        df = df.exclude(
-            "Content-Length",
-            "warc_headers",
-            "WARC-Identified-Payload-Type",
-            "WARC-Date",
-            "WARC-Type",
-        )
-        df = df.with_column(
-            "links",
-            col("warc_content").apply(
-                self.parse_content,
-                return_dtype=daft.DataType.list(
-                    daft.DataType.struct(
-                        {
-                            "url": daft.DataType.string(),
-                            "alt": daft.DataType.string(),
-                            "hash": daft.DataType.string(),
-                        }
-                    )
-                ),
-            ),
-        )
-        df = df.exclude("warc_content")
-        df = df.explode("links")
-        df = df.with_columns(
-            {
-                "url": col("links")["url"],
-                "alt": col("links")["alt"],
-                "hash": col("links")["hash"],
-            }
-        )
-        df = df.where(col("links").not_null())
-        df = df.exclude("links")
-        df = df.drop_duplicates("hash")
-        return df
-
+class AudioExtractor(Extractor):
     def valid_audio_link(self, link):
         valid_audio = any(
             link.get("url", "").endswith(ext)
@@ -55,51 +9,10 @@ class Audios:
         )
         return valid_audio
 
-    def extract_audio_from_links(self, links):
+    def extract_from_links(self, links):
         filtered_links = [
             {"url": link["url"], "alt": link.get("text", "")}
             for link in links
             if self.valid_audio_link(link)
         ]
         return filtered_links
-
-    def make_link_absolute(self, url, base_url):
-        if url.startswith("http://") or url.startswith("https://"):
-            return url
-        try:
-            return urljoin(base_url, url)
-        except ValueError:
-            return url
-
-    def make_links_absolute(self, links, base_url):
-        return [
-            {"url": self.make_link_absolute(link["url"], base_url), "alt": link["alt"]}
-            for link in links
-        ]
-
-    def parse_content(self, record):
-        all_links = []
-        record_data = simdjson.loads(record)
-        envelope = record_data["Envelope"]
-        payload = envelope["Payload-Metadata"]
-        if "HTTP-Response-Metadata" not in payload:
-            return
-        http_resp = payload["HTTP-Response-Metadata"]
-        if "HTML-Metadata" not in http_resp:
-            return
-        metadata = http_resp["HTML-Metadata"]
-        if "Links" not in metadata:
-            return
-        links = metadata["Links"]
-        base_url = envelope["WARC-Header-Metadata"]["WARC-Target-URI"]
-        filtered_links = self.extract_audio_from_links(links)
-        filtered_links = self.make_links_absolute(filtered_links, base_url)
-        filtered_links = [
-            link
-            for link in filtered_links
-            if link["url"].startswith("http://") or link["url"].startswith("https://")
-        ]
-        for link in filtered_links:
-            link["hash"] = xxhash.xxh128((link["alt"] + link["url"])).hexdigest()
-        all_links.extend(filtered_links)
-        return all_links
